a16c06a282f23a7225932152c6402f769fa01e8c
Christian Seifert <perdian@perdian.de>
2024-11-21 10:29:34 +0100

    Extend structure

--- backend/application/src/main/java/de/perdian/yavin/backend/application/Application.java
+++ backend/application/src/main/java/de/perdian/yavin/backend/application/Application.java
@@ -2,8 +2,10 @@ package de.perdian.yavin.backend.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "de.perdian.yavin.backend")
 public class Application {
 
     public static void main(String[] args) {
---
+++ backend/development/src/main/java/de/perdian/yavin/backend/development/DevelopmentApplication.java
@@ -0,0 +1,12 @@
+package de.perdian.yavin.backend.development;
+
+import de.perdian.yavin.backend.application.Application;
+import org.springframework.boot.SpringApplication;
+
+public class DevelopmentApplication extends Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentApplication.class, args);
+    }
+
+}
